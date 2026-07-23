ce8bec671d808e135f030cf5798ba125d86cdadc
A-Cobra <conradbravina@gmail.com>
2026-07-23 14:15:44 -0500

    Changing the next config for hosting

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Outputs a static 'out' folder upon building
 };
 
 export default nextConfig;
